f479fd4c786cf7f22e28a2bd11549277ee0cb5ee
l3gacyb3ta <l3gacy.b3ta@disroot.org>
2021-06-27 13:31:09 -0400

    Basic site

---
+++ components/head.js
@@ -0,0 +1,7 @@
+export default function Meta() {
+    return (
+        <head>
+            <link rel="stylesheet" href="https://unpkg.com/blocks.css/dist/blocks.min.css" />
+        </head>
+    )
+}
